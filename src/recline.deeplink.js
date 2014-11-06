b719519fd180b53e7dd2f394c5d581ba89905717
Mariano Carballal <marianocarballal@gmail.com>
2014-11-06 10:45:53 -0300

    Better urls. Adding smart object diff.

--- src/recline.deeplink.js
+++ src/recline.deeplink.js
@@ -11,10 +11,12 @@ this.recline.DeepLink = this.recline.DeepLink || {};
     var currentView = null;
     var router;
     var changes = {};
+    var parser = new my.Parser();
+    var deep = DeepDiff.noConflict();
+    var firstState = _.clone(_.omit(multiview.state.attributes, 'dataset'));
 
     self.updateState = function(state){
       var multiviewState = self.transform(state, self.toState);
-
       changes = multiviewState || {};
       if (multiviewState) {
         multiviewState = _.extend(multiview.state.attributes, multiviewState);
@@ -27,6 +29,8 @@ this.recline.DeepLink = this.recline.DeepLink || {};
           pageView.view.state.set(multiviewState[viewKey]);
           if(typeof pageView.view.redraw === 'function' && pageView.id === 'graph'){
             setTimeout(pageView.view.redraw, 0);
+          } else if(pageView.id === 'grid') {
+            pageView.view.render();
           }
         });
       }
@@ -37,25 +41,64 @@ this.recline.DeepLink = this.recline.DeepLink || {};
       try{
         result = transformFunction(input);
       } catch(e){
+        console.log(e);
         result = null;
       }
       return result;
     };
 
     self.toState = function(serializedState){
-      return JSON.parse(decodeURI(serializedState));
+      var stringObject = parser.inflate(decodeURI(serializedState));
+      return JSON.parse(stringObject);
     };
 
     self.toParams = function(state){
-      return encodeURI(JSON.stringify(_.omit(state.attributes, 'dataset')));
+      var stringObject = JSON.stringify(_.omit(state.attributes, 'dataset'));
+      return parser.compress(stringObject);
     };
 
     self.onStateChange = function(event){
+      var ch = deep.diff(firstState, _.omit(multiview.state.attributes, 'dataset'));
+      var tempChanges = {};
+      _.each(ch, function(c){
+        if(c.kind === 'E'){
+          self.createNestedObject(tempChanges, c.path, c.rhs);
+        } else if(c.kind === 'A') {
+          self.createNestedObject(tempChanges, c.path, c);
+        }
+      });
+      changes = _.extend(changes, tempChanges);
       var newState = new recline.Model.ObjectState();
-      changes = self.saveChanges(multiview.state.changed, changes);
       newState.attributes = changes;
       router.navigate(self.transform(newState, self.toParams));
       self.updateControls();
+    };
+
+    self.createNestedObject = function( base, prop, value ) {
+        var names = _.clone(prop);
+        var lastName = arguments.length === 3 ? names.pop() : false;
+
+        for( var i = 0; i < names.length; i++) {
+            base = base[names[i]] = base[names[i]] || {};
+        }
+
+        if(lastName && !_.isArray(value) && !_.isObject(value)){
+          base = base[lastName] = value;
+        }
+
+        if(_.isObject(value) && value.kind === 'A'){
+          if(_.isUndefined(base[lastName])){
+            base[lastName] = [];
+          }
+          if(value.item.kind == 'N'){
+            base = base[lastName][value.index] = value.item.rhs;
+          }
+          if(value.item.kind == 'D'){
+            base[lastName].splice(value.index, value.item.rhs);
+            base = base[lastName];
+          }
+        }
+        return base;
     };
 
     self.saveChanges = function(delta, changes){
@@ -112,5 +155,63 @@ this.recline.DeepLink = this.recline.DeepLink || {};
     self.initialize();
   };
 
+  my.Parser = function(){
+    var self = this;
+    var compressMap = {
+      'backend':'b',
+      'currentView': 'c',
+      'dataset':'d',
+      'fields': 'f',
+      'records': 'r',
+      'query': 'qy',
+      'facets': 'fc',
+      'filters':'fl',
+      'from': 'fr',
+      'q':'q',
+      'size':'sz',
+      'readOnly':'ro',
+      'url':'ul',
+      'view-graph': 'vga',
+      'graphType': 'gt',
+      'group': 'gp',
+      'series': 'sr',
+      'view-grid':'vgi',
+      'columnsEditor': 'ce',
+      'columnsOrder': 'co',
+      'columnsSort': 'cs',
+      'columnsWith':'cw',
+      'fitColumns': 'fcm',
+      'gridOptions': 'go',
+      'hiddenColumns': 'hc',
+      'options':'op',
+      'view-map':'vm',
+      'autoZoom': 'az',
+      'cluster': 'cl',
+      'geomField': 'gf',
+      'latField': 'laf',
+      'lonField': 'lof',
+    };
+
+    self.compress = function(str){
+      //replace words
+      //remove start and end brackets
+      //replace true by 1 and false by 0
+      return self.escapeStrings(str);
+    };
+
+    self.inflate = function(str){
+      return self.parseStrings(str);
+    };
+
+    self.escapeStrings = function(str){
+      str = str.replace(/"([a-zA-Z-]+)"\s?:/g ,  "$1:");
+      return str.replace(/"([a-zA-Z-#]+)"/g ,  "!$1");
+    };
+
+    self.parseStrings = function(str){
+      str = str.replace(/([a-zA-Z-]+)\s?:/g ,  "\"$1\":");
+      return str.replace(new RegExp('!([a-zA-Z-#]+)', 'g'),  "\"$1\"");
+    };
+  };
 
 })(jQuery, this.recline.DeepLink);
